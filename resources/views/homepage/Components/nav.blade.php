fea89bb4b5b91f96eb90d1586ad9d72378057065
Rheyan <rjvblanco.chmsu@gmail.com>
2024-03-05 17:12:22 +0800

    photos

--- resources/views/homepage/Components/nav.blade.php
+++ resources/views/homepage/Components/nav.blade.php
@@ -2,7 +2,7 @@
     <nav class="navbar navbar-expand-lg bg-white navbar-light py-0 px-4">
         <a href="{{ route('home') }}" class="navbar-brand d-flex align-items-center text-center c_primary">
             <div class="icon p-2 me-2">
-                <img class="img-fluid" src="img/os_logo.png" alt="Icon" style="width: 30px; height: 30px;">
+                <img class="img-fluid" src="img/os_logo.png" alt="Icon" style="width: 60px; height: 60px;">
             </div>
             <h1 class="m-0 text-primary">Orange Shire</h1>
         </a>
