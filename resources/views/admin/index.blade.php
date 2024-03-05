b2fc21bf853dae2525dc5829f6c343b67d423ec4
JpUbas <jpubas@gmail.com>
2024-03-05 16:26:06 +0800

    asset

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -730,16 +730,16 @@
     <!-- Warning Section Ends -->
 
     <!-- Required Js -->
-    <script src="assets/js/vendor-all.min.js"></script>
-    <script src="assets/js/plugins/bootstrap.min.js"></script>
+    <script src="{{asset('assets/js/vendor-all.min.js')}}"></script>
+    <script src="{{asset('assets/js/plugins/bootstrap.min.js')}}"></script>
 
 <!-- Apex Chart -->
-<script src="assets/js/plugins/apexcharts.min.js"></script>
+<script src="{{asset('assets/js/plugins/apexcharts.min.js')}}"></script>
 
 
 <!-- custom-chart js -->
-<script src="assets/js/pages/dashboard-main.js"></script>
-<script src="assets/js/pcoded.min.js"></script>
+<script src="{{asset('assets/js/pages/dashboard-main.js')}}"></script>
+<script src="{{asset('assets/js/pcoded.min.js')}}"></script>
 
 </body>
 
