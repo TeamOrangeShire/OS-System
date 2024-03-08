affa953c848dcfc047db55edaabbedd861cb8262
tish <tbtizon.chmsu@gmail.com>
2024-03-08 19:46:50 +0800

    updated rooms icons

--- resources/views/admin/admin_lockscreen.blade.php
+++ resources/views/admin/admin_lockscreen.blade.php
@@ -4,6 +4,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="icon" href="{{asset('assets/images/os_logo.png')}}" type="image/x-icon">
+
   <title>Lock Screen</title>
   <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
   <style>
