b7210613e602cc8d2bd8bae90d9ed539744ef74f
JpUbas <jpubas@gmail.com>
2024-03-11 18:32:46 +0800

    profile unfinish edit

--- resources/views/admin/admin_profile.blade.php
+++ resources/views/admin/admin_profile.blade.php
@@ -46,30 +46,54 @@
 <div class="pcoded-main-container">
     <div class="pcoded-content">
         <!-- [ Main Content start ] start -->
-      
+      @php
+          $admin_name = App\Models\AdminAcc::where('admin_id',session('Admin_id'))->first();
+          $fullname = $admin_name->admin_firstname.' '.$admin_name->admin_middlename[0].'. '.$admin_name->admin_lastname;
+      @endphp
+      <form action="">
         <div class="container rounded bg-white mt-5 mb-5">
             <div class="row">
                 <div class="col-md-6 border-right">
                     <div class="d-flex flex-column align-items-center text-center p-3 py-5"><img class="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg">
-                        <span class="font-weight-bold">Username</span>
-                        <span class="text-black-50">Fullname</span>
+                        <span class="font-weight-bold">{{$admin_name->admin_username}}</span>
+                        <span class="text-black-50">{{$fullname}}</span>
                         <span> </span>
                     </div>
                 </div>
+              
                 <div class="col-md-5 ">
                     <div class="p-3 py-5">
                         <div class="d-flex justify-content-between align-items-center mb-3">
                             <h4 class="text-right">Profile Settings</h4>
                         </div>
                         <div class="row mt-2">
-                            <div class="col-md-6"><label class="labels">Full Name</label><input type="text" class="form-control" placeholder="Full Name" value=""></div>
-                            <div class="col-md-6">
-                                <label class="labels">Profile Image</label><input type="file" class="form-control" value="" placeholder="surname">
-                            </div>
+                            <div class="col-md-6"><label class="labels">First Name</label><input type="text" class="form-control" name="firstname" placeholder="Full Name" value="{{$admin_name->admin_firstname}}"></div>
+                            <div class="col-md-6"><label class="labels">Middle Name</label><input type="text" class="form-control" name="middlename" placeholder="Full Name" value="{{$admin_name->admin_middlename}}"></div>
+                            
                         </div>
                         <div class="row mt-2">
-                            <div class="col-md-6"><label class="labels">Username</label><input type="text" class="form-control" placeholder="Username" value=""></div>
-                            <div class="col-md-6"><label class="labels">Password</label><input type="password" class="form-control" value="" placeholder="Password"></div>
+                            <div class="col-md-6"><label class="labels">Last Name</label><input type="text" class="form-control" name="lastname" placeholder="Full Name" value="{{$admin_name->admin_lastname}}"></div>
+                            <div class="col-md-6"> <label for="exampleInputEmail1">Ext.</label>
+                                <select class="form-control" id="exampleFormControlSelect1" name="ext">
+                                    <option value="{{$admin_name->admin_ext}}">{{$admin_name->admin_ext}}</option>
+                                    <option value="Jr.">Jr.</option>
+                                    <option value="Sr.">Sr.</option>
+                                    <option value="II">II</option>
+                                    <option value="III">III</option>
+                                    <option value="IV">IV</option>
+                                    <option value="V">V</option>
+                                    <option value="Jra.">Jra.</option>
+                                    <option value="Esq.">Esq.</option>
+                                </select>     </div>
+                            
+                        </div>
+                        <div class="row mt-2">
+                            <div class="col-md-6"><label class="labels">Username</label><input type="text" class="form-control" name="username" placeholder="Username" value="{{$admin_name->admin_username}}"></div>
+                            <div class="col-md-6"><label class="labels">New Password</label><input type="password" class="form-control" name="password" value="" placeholder="Password"></div>
+                        </div>
+                        <div class="row mt-2">
+                    
+                            <div class="col-md-6"><label class="labels">Old Password</label><input type="password" name="password2" class="form-control" value="" placeholder="Password"></div>
                         </div>
                         
                      
@@ -78,11 +102,10 @@
                         </div>
                     </div>
                 </div>
-                
+          
             </div>
         </div>
-        </div>
-        </div>
+    </form>
        
         <!-- [ Main Content ] end -->
     </div>
