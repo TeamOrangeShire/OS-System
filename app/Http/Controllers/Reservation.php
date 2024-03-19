ddaeb42718271cf34dfb76c8bef4bedc3dacc6d8
Rheyan <rjvblanco.chmsu@gmail.com>
2024-03-19 18:48:52 +0800

    Still Bugs in the reservation Page

--- app/Http/Controllers/Reservation.php
+++ app/Http/Controllers/Reservation.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
+use App\Models\Reservations;
+use App\Models\CustomerAcc;
 
 class Reservation extends Controller
 {
@@ -12,5 +14,36 @@ class Reservation extends Controller
       $date = $request->date;
       Session::put('selectedDate', $date);
       return redirect()->route('book');
+    }
+
+    public function SaveReservation(Request $req){
+      $customer = $req->customer_id;
+      $company = $req->company_name;
+      $contact = $req->contact;
+      $r_dur_price = $req->duration;
+      $date = $req->date;
+      $time = $req->time;
+      $start = $time[0];
+      $end = $time[2];
+     
+      $res = new Reservations();
+      $res->customer_id = $customer;
+      $res->rprice_id = $r_dur_price;
+      $res->res_date = $date;
+      $res->res_company = $company;
+      $res->res_start = $start;
+      $res->res_end = $end;
+      $res->res_notes = '';
+      $res->res_status = '';
+      $res->res_disable = 0;
+      $res->save();
+
+      $cust = CustomerAcc::where('customer_id', $customer)->first();
+      $cust->update([
+        'customer_phone_num'=>$contact,
+      ]);
+
+      return response()->json(['status'=>'success']);
+
     }
 }
