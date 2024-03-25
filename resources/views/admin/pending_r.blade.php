4534575b6e4b0dc65ce28a4b784d6b584b9dcb38
JpUbas <jpubas@gmail.com>
2024-03-25 14:12:43 +0800

    commit

--- resources/views/admin/pending_r.blade.php
+++ resources/views/admin/pending_r.blade.php
@@ -60,9 +60,10 @@
                                
                                     <th>Name</th>
                                     <th>Email</th>
+                                    <th>Number</th>
+                                    <th>Room No.</th>
                                     <th>Date</th>
                                     <th>Time</th>
-                                    <th>Room No.</th>
                                     <th colspan="2"> Action Buttons</th>
 
 
@@ -71,7 +72,7 @@
                             </thead>
                             <tbody>
                                 @php
-                                $Reservation = App\Models\Reservations::all();
+                                $Reservation = App\Models\Reservations::where('res_status',0)->get();
                             @endphp
                             @foreach ($Reservation as $res)
                                 
@@ -79,15 +80,29 @@
                                
                                 <td>@php
                                     $cus_id = $res->customer_id;
+                                    
                                     $cus_info = App\Models\CustomerAcc::where('customer_id',$cus_id)->first();
+                                    $full_name = $cus_info->customer_firstname.' '.$cus_info->customer_lastname;
+                                    $time = $res->res_start.'-'.$res->res_end;
                                 @endphp
-                                {{$cus_info->customer_firstname}}</td>
-                                <td>mark@gmail.com</td>
-                                <td>02/04/2024</td>
-                                <td>10PM TO 14PM</td>
-                                <td>1</td>
+                                {{$full_name}} </td>
+                                <td>{{$cus_info->customer_email}}</td>
+                                <td>{{$cus_info->customer_phone_num}}</td>
+                                <td>@php
+                                    $rprice_id = $res->rprice_id;
+                                    $rprice_info = App\Models\RoomPricing::where('rprice_id',$rprice_id)->first();
+                                    $room_id = $rprice_info->room_id;
+                                    $room_info = App\Models\Rooms::where('room_id',$room_id)->first();
+                                    $room_name = $room_info->room_number;
+                                    $timeplace = FilterTime($time);
+                                @endphp
+                                {{$room_name}}
+                                 </td>
+                                <td>{{$res->res_date}}</td>
+                                <td>{{$timeplace}}</td>
+                               
                                 <td> 
-                                    <button type="button" class="btn btn-success" data-toggle="modal" data-target="#confirmmodal"  onclick="updatemodal(`{{$res->res_id}}`"><i class="feather icon-check-circle"></i></button>  
+                                    <button type="button" class="btn btn-success" data-toggle="modal" data-target="#confirmmodal"  onclick="confirmres(`{{$res->res_id}}`,'{{$full_name}}','{{$timeplace}}','{{$room_name}}','{{$res->res_date}}')"><i class="feather icon-check-circle"></i></button>  
                                     <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#declinemodal"><i class="feather icon-x-circle"></i></button>   </td>
                               </tr>
 
@@ -107,24 +122,28 @@
 <div id="confirmmodal" class="modal fade" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
     <div class="modal-dialog modal-dialog-centered" role="document">
         <div class="modal-content">
+            <form action="{{route('ConfirmReservation')}}" method="POST">
+                @csrf
+          
             <div class="modal-header">
                 <h5 class="modal-title" id="exampleModalCenterTitle">Confirm Reservation?</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
             </div>
             <div class="modal-body">
                 <div class="col-md-12">
-                    <p>Name: <span id="subscriptionName"></span></p>
-                    <p>Email: <span id="subscriptionPlan"></span></p>
-                    <p>Date: <span id="customerFullName"></span></p>
-                    <p>Time: <span id="customerFullName"></span></p>
-                    <p>Room: <span id="customerFullName"></span></p>
+                    <input type="hidden" name="r_id" id="r_id" style="border: none;">
+                    <p>Name: <input type="text" name="CusName" id="CusName" style="border: none;"></p>
+                    <p>Date: <input type="text" name="" id="Rdate" style="border: none;"></p>
+                    <p>Time: <input type="text" name="" id="Rtime" style="border: none;"></p>
+                    <p>Room: <input type="text" name="" id="Rroom" style="border: none;"></p>
 
                     <div style="text-align: center;">
-                        <button type="button" class="btn btn-primary" onclick="confirmDisable()">Yes</button>
-                        <button type="button" class="btn btn-secondary" onclick="cancel()">No</button>
+                        <button type="submit" class="btn btn-primary" >Yes</button>
+                        <button type="button" class="btn btn-secondary">No</button>
                     </div>
                 </div>
             </div>
+        </form>
         </div>
     </div>
 </div>
@@ -167,7 +186,16 @@
 {{-- modal end --}}
 
 <!-- [ Main Content ] end -->
-
+    <script>
+           function confirmres(id,name,time,room,date){
+            document.getElementById('r_id').value=id;
+            document.getElementById('CusName').value=name;
+            document.getElementById('Rtime').value=time;
+            document.getElementById('Rroom').value=room;
+            document.getElementById('Rdate').value=date;
+          
+        }
+    </script>
     <!-- Required Js -->
     <script src="{{asset('js/main.js')}}"></script>
 
