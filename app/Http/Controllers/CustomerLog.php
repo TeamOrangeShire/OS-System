480d96baced74075439e216452c2f55b745d6688
JpUbas <jpubas@gmail.com>
2024-04-06 17:47:42 +0800

    log accept

--- app/Http/Controllers/CustomerLog.php
+++ app/Http/Controllers/CustomerLog.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 use App\Models\CustomerLogs;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class CustomerLog extends Controller
 {
@@ -13,5 +14,20 @@ class CustomerLog extends Controller
     $log = CustomerLogs::where('customer_id',$id)->get();
 
     return response()->json(['logs'=>$log]);
+  }
+  public function acceptLog(Request $request){
+
+    $id = $request->pending_log;
+    $currentTime = Carbon::now()->setTimezone('Asia/Hong_Kong')->format('h:i A');
+
+    $log = CustomerLogs::where('log_id',$id)->first();
+    
+    $log->update([
+
+        'log_end_time'=>$currentTime,
+        'log_status'=> 2,
+
+    ]);
+    return response()->json(['status'=> 'success']);
   }
 }
