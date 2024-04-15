205a54f90a9592a609e69bc16a1201e0749fb4de
Rheyan <rjvblanco.chmsu@gmail.com>
2024-04-15 15:55:25 +0800

    String to integer conversion

--- app/helpers.php
+++ app/helpers.php
@@ -133,218 +133,207 @@ function parseTime($time) {
     return $totalMinutes * 60 * 1000; 
 }
 
-function PaymentCalc($start, $end, $type) {
-    $time = timeDifference($start, $end);
-    $hours = $time['hours'];
-    $minutes = $time['minutes'];
-  $payment = 0;
-  if ($type === "Students" || $type === "Teachers" || $type === "Reviewers") {
-      switch ($hours) {
-          case 1:
-              $payment += 50;
-              break;
-          case 2:
-              $payment += 100;
-              break;
-          case 3:
-              $payment += 140;
-              break;
-          case 4:
-              $payment += 185;
-              break;
-          case 5:
-              $payment += 220;
-              break;
-          case 6:
-              $payment += 240;
-              break;
-          case 7:
-              $payment += 280;
-              break;
-          case 8:
-              $payment += 320;
-              break;
-          case 0:
-              $payment += 0;
-              break;
-          default:
-              $payment += 320;
-              break;
-      }
-      if ($hours === 0 && $minutes <= 15) {
+function PaymentCalc($hoursString, $minutesString, $type){
+    $hours = intval($hoursString);
+    $minutes = intval($minutesString);
+    $payment = 0;
+    if($type === "Students" || $type === "Teachers" || $type === "Reviewers"){
+      switch($hours){
+        case 1:
+          $payment += 50;
+          break;
+        case 2:
+          $payment += 100;
+          break;
+        case 3:
+          $payment += 140;
+          break;
+        case 4:
+          $payment += 185;
+          break;
+        case 5:
+          $payment += 220;
+          break;
+        case 6:
+          $payment += 240;
+          break;
+        case 7:
+          $payment += 280;
+          break;
+        case 8:
+          $payment += 320;
+          break;
+        case 0:
           $payment += 0;
+          break;
+        default:
+          $payment += 320;
+          break;
       }
-      if (($hours === 0 || $hours === 1) && ($minutes > 15 && $minutes <= 45)) {
-          $payment += 25;
+      if($hours === 0 && $minutes <= 15){
+        $payment += 0;
       }
-      if (($hours === 0 || $hours === 1) && ($hours < 2 && $minutes > 45)) {
+      if( ($hours === 0 || $hours === 1) && ($minutes > 15 && $minutes <= 45) ){
+        $payment += 25;
+      }
+      if(($hours === 0 || $hours === 1) && ($hours < 2 && $minutes > 45) ){
           $payment += 50;
       }
-      if ($hours === 2 && $minutes < 15) {
+      if($hours === 2 && $minutes < 15){
+        $payment += 0;
+      }
+      if($hours === 2 && $minutes > 15 && $minutes <= 45){
+        $payment += 20;
+      }
+      if($hours === 2 && $hours < 3 && $minutes > 45){
+        $payment += 40;
+      }
+      if($hours === 3 && $minutes < 15){
+        $payment += 0;
+      }
+      if($hours === 3 && $minutes > 15 && $minutes <= 45){
+        $payment += 20;
+      }
+      if($hours === 3 && $hours < 4 && $minutes > 45){
+        $payment += 45;
+      }
+      if($hours === 4 && $minutes < 15){
+        $payment += 0;
+      }
+      if($hours === 4 && $minutes > 15 && $minutes <= 45){
+        $payment += 25;
+      }
+      if($hours === 4 && $hours < 5 && $minutes > 45){
+        $payment += 35;
+      }
+      if($hours === 5 && $minutes < 15){
+        $payment += 0;
+      }
+      if($hours === 5 && $minutes > 15 && $minutes <= 45){
+        $payment += 10;
+      }
+      if($hours === 5 && $hours < 6 && $minutes > 45){
+        $payment += 20;
+      }
+      if($hours === 6 && $minutes < 15){
+        $payment += 0;
+      }
+      if($hours === 6 && $minutes > 15 && $minutes <= 45){
+        $payment += 20;
+      }
+      if($hours === 6 && $hours < 7 && $minutes > 45){
+        $payment += 40;
+      }
+      if($hours === 7 && $minutes < 15){
+        $payment += 0;
+      }
+      if($hours === 7 && $minutes > 15 && $minutes <= 45){
+        $payment += 20;
+      }
+      if($hours === 7 && $hours < 8 && $minutes > 45){
+        $payment += 40;
+      }
+    }else{
+      switch($hours){
+        case 1:
+          $payment += 80;
+          break;
+        case 2:
+          $payment += 160;
+          break;
+        case 3:
+          $payment += 200;
+          break;
+        case 4:
+          $payment += 260;
+          break;
+        case 5:
+          $payment += 280;
+          break;
+        case 6:
+          $payment += 300;
+          break;
+        case 7:
+          $payment += 350;
+          break;
+        case 8:
+          $payment += 400;
+          break;
+        case 0:
           $payment += 0;
+          break;
+        default:
+          $payment += 400;
+          break;
       }
-      if ($hours === 2 && $minutes > 15 && $minutes <= 45) {
-          $payment += 20;
+      if($hours === 0 && $minutes <= 15){
+        $payment += 0;
       }
-      if ($hours === 2 && $hours < 3 && $minutes > 45) {
-          $payment += 40;
+      if( ($hours === 0 || $hours === 1) && ($minutes > 15 && $minutes <= 45) ){
+        $payment += 40;
       }
-
-      if ($hours === 3 && $minutes < 15) {
-          $payment += 0;
-      }
-      if ($hours === 3 && $minutes > 15 && $minutes <= 45) {
-          $payment += 20;
-      }
-      if ($hours === 3 && $hours < 4 && $minutes > 45) {
-          $payment += 45;
-      }
-
-      if ($hours === 4 && $minutes < 15) {
-          $payment += 0;
-      }
-      if ($hours === 4 && $minutes > 15 && $minutes <= 45) {
-          $payment += 25;
-      }
-      if ($hours === 4 && $hours < 5 && $minutes > 45) {
-          $payment += 35;
-      }
-
-      if ($hours === 5 && $minutes < 15) {
-          $payment += 0;
-      }
-      if ($hours === 5 && $minutes > 15 && $minutes <= 45) {
-          $payment += 10;
-      }
-      if ($hours === 5 && $hours < 6 && $minutes > 45) {
-          $payment += 20;
-      }
-
-      if ($hours === 6 && $minutes < 15) {
-          $payment += 0;
-      }
-      if ($hours === 6 && $minutes > 15 && $minutes <= 45) {
-          $payment += 20;
-      }
-      if ($hours === 6 && $hours < 7 && $minutes > 45) {
-          $payment += 40;
-      }
-
-      if ($hours === 7 && $minutes < 15) {
-          $payment += 0;
-      }
-      if ($hours === 7 && $minutes > 15 && $minutes <= 45) {
-          $payment += 20;
-      }
-      if ($hours === 7 && $hours < 8 && $minutes > 45) {
-          $payment += 40;
-      }
-  } else {
-      switch ($hours) {
-          case 1:
-              $payment += 80;
-              break;
-          case 2:
-              $payment += 160;
-              break;
-          case 3:
-              $payment += 200;
-              break;
-          case 4:
-              $payment += 260;
-              break;
-          case 5:
-              $payment += 280;
-              break;
-          case 6:
-              $payment += 300;
-              break;
-          case 7:
-              $payment += 350;
-              break;
-          case 8:
-              $payment += 400;
-              break;
-          case 0:
-              $payment += 0;
-              break;
-          default:
-              $payment += 400;
-              break;
-      }
-      if ($hours === 0 && $minutes <= 15) {
-          $payment += 0;
-      }
-      if (($hours === 0 || $hours === 1) && ($minutes > 15 && $minutes <= 45)) {
-          $payment += 40;
-      }
-      if (($hours === 0 || $hours === 1) && ($hours < 2 && $minutes > 45)) {
+      if(($hours === 0 || $hours === 1) && ($hours < 2 && $minutes > 45) ){
           $payment += 80;
       }
-      if ($hours === 2 && $minutes < 15) {
-          $payment += 0;
+      if($hours === 2 && $minutes < 15){
+        $payment += 0;
       }
-      if ($hours === 2 && $minutes > 15 && $minutes <= 45) {
-          $payment += 30;
+      if($hours === 2 && $minutes > 15 && $minutes <= 45){
+        $payment += 30;
       }
-      if ($hours === 2 && $hours < 3 && $minutes > 45) {
-          $payment += 40;
+      if($hours === 2 && $hours < 3 && $minutes > 45){
+        $payment += 40;
       }
-
-      if ($hours === 3 && $minutes < 15) {
-          $payment += 0;
+      if($hours === 3 && $minutes < 15){
+        $payment += 0;
       }
-      if ($hours === 3 && $minutes > 15 && $minutes <= 45) {
-          $payment += 30;
+      if($hours === 3 && $minutes > 15 && $minutes <= 45){
+        $payment += 30;
       }
-      if ($hours === 3 && $hours < 4 && $minutes > 45) {
-          $payment += 60;
+      if($hours === 3 && $hours < 4 && $minutes > 45){
+        $payment += 60;
       }
-
-      if ($hours === 4 && $minutes < 15) {
-          $payment += 0;
+      if($hours === 4 && $minutes < 15){
+        $payment += 0;
       }
-      if ($hours === 4 && $minutes > 15 && $minutes <= 45) {
-          $payment += 10;
+      if($hours === 4 && $minutes > 15 && $minutes <= 45){
+        $payment += 10;
       }
-      if ($hours === 4 && $hours < 5 && $minutes > 45) {
-          $payment += 20;
+      if($hours === 4 && $hours < 5 && $minutes > 45){
+        $payment += 20;
       }
-
-      if ($hours === 5 && $minutes < 15) {
-          $payment += 0;
+      if($hours === 5 && $minutes < 15){
+        $payment += 0;
       }
-      if ($hours === 5 && $minutes > 15 && $minutes <= 45) {
-          $payment += 10;
+      if($hours === 5 && $minutes > 15 && $minutes <= 45){
+        $payment += 10;
       }
-      if ($hours === 5 && $hours < 6 && $minutes > 45) {
-          $payment += 20;
+      if($hours === 5 && $hours < 6 && $minutes > 45){
+        $payment += 20;
       }
-
-      if ($hours === 6 && $minutes < 15) {
-          $payment += 0;
+      if($hours === 6 && $minutes < 15){
+        $payment += 0;
       }
-      if ($hours === 6 && $minutes > 15 && $minutes <= 45) {
-          $payment += 30;
+      if($hours === 6 && $minutes > 15 && $minutes <= 45){
+        $payment += 30;
       }
-      if ($hours === 6 && $hours < 7 && $minutes > 45) {
-          $payment += 50;
+      if($hours === 6 && $hours < 7 && $minutes > 45){
+        $payment += 50;
       }
-
-      if ($hours === 7 && $minutes < 15) {
-          $payment += 0;
+      if($hours === 7 && $minutes < 15){
+        $payment += 0;
       }
-      if ($hours === 7 && $minutes > 15 && $minutes <= 45) {
-          $payment += 20;
+      if($hours === 7 && $minutes > 15 && $minutes <= 45){
+        $payment += 20;
       }
-      if ($hours === 7 && $hours < 8 && $minutes > 45) {
-          $payment += 50;
+      if($hours === 7 && $hours < 8 && $minutes > 45){
+        $payment += 50;
       }
+    }
+  
+    return $payment;
   }
-
-  return $payment;
-}
-
+  
   
 
 ?>
