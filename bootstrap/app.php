a423f6248de72bff889f086e44e5bc9a262abf7c
Rheyan <rjvblanco.chmsu@gmail.com>
2024-04-17 14:27:51 +0800

    Test

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -14,7 +14,9 @@
 $app = new Illuminate\Foundation\Application(
     $_ENV['APP_BASE_PATH'] ?? dirname(__DIR__)
 );
-
+$app->bind('path.public', function () {
+    return __DIR__.'/../public_html';
+});
 /*
 |--------------------------------------------------------------------------
 | Bind Important Interfaces
