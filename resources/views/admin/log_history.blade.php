defe368313c72a8b33f371f1677c2d52bb68d3e5
JpUbas <jpubas@gmail.com>
2024-04-19 11:02:06 +0800

    update logggggg

--- resources/views/admin/log_history.blade.php
+++ resources/views/admin/log_history.blade.php
@@ -200,9 +200,8 @@
                     </p>
                 </div>
                 <div class="tab-pane fade" id="contact" role="tabpanel" aria-labelledby="contact-tab">
-                    <p class="mb-0">Etsy mixtape wayfarers, ethical wes anderson tofu before they sold out mcsweeney's organic lomo retro fanny pack lo-fi farm-to-table readymade. Messenger bag gentrify pitchfork tattooed
-                        craft beer,
-                        iphone skateboard locavore carles etsy salvia banksy hoodie helvetica. DIY synth PBR banksy irony. Lnyl craft beer blog stumptown. Pitchfork sustainable tofu synth chambray yr.</p>
+                    {{-- content --}}
+                    {{-- end content --}}
                 </div>
             </div>
         </div>
