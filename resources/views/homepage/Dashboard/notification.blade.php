076c64668601de383e58c48050b54e235a57463f
Rheyan <rjvblanco.chmsu@gmail.com>
2024-05-04 18:34:08 +0800

    Notification Display Update

--- resources/views/homepage/Dashboard/notification.blade.php
+++ resources/views/homepage/Dashboard/notification.blade.php
@@ -29,13 +29,12 @@
 
 
     @php
-        $notif = App\Models\CustomerNotification::where('user_id', $user_id)->where('user_type', 'Customer');
-        $notifUnread = $notif->where('notif_status', 0)->orderBy('created_at', 'desc')->get();
-        $notifRead = $notif->where('notif_status', 1)->orderBy('created_at', 'desc')->get();
+        $notification = App\Models\CustomerNotification::where('user_id', $user_id)->where('user_type', 'Customer')->get();
+        
     @endphp
           <!-- List group with Advanced Contents -->
           <div class="list-group">
-            @foreach ($notifUnread as $notif)
+            @foreach ($notification as $notif)
             @php
                 $pastTime = PastTimeCalc($notif->created_at);
 
@@ -46,25 +45,16 @@
                     $calcTime = $pastTime[0]. " hrs, ". $calcMinutes . "mins ago";
                 }
             @endphp
-            <button class="list-group-item list-group-item-action active" aria-current="true">
+            <button class="list-group-item list-group-item-action {{ $notif->notif_status === 0? '' : 'active' }}" {{ $notif->notif_status === 0 ? '' : 'aria-current="true"' }}>
                 <div class="d-flex w-100 justify-content-between">
                   <h5 class="mb-1">{!! $notif->notif_header !!}</h5>
                   <small>{{ $calcTime }}</small>
                 </div>
                 <p class="mb-1">{!! substr($notif->notif_message,0, 40). "...." !!}</p>
-                <small>Unread</small>
+                <small>{{ $notif->notif_status === 0 ? 'Unread' : 'Read' }}</small>
             </button>
             @endforeach
-            @foreach ($notifRead as $notif)
-            <a href="#" class="list-group-item list-group-item-action">
-                <div class="d-flex w-100 justify-content-between">
-                  <h5 class="mb-1">List group item heading</h5>
-                  <small class="text-muted">3 days ago</small>
-                </div>
-                <p class="mb-1">Some placeholder content in a paragraph.</p>
-                <small class="text-muted">And some muted small print.</small>
-              </a>
-            @endforeach
+         
 
      
 
