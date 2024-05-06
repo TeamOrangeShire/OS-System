a73b911270efc618a7a1698e48c0ebc13348e5ba
JpUbas <jpubas@gmail.com>
2024-05-06 11:18:33 +0800

    customer_log

--- app/Http/Controllers/CustomerLog.php
+++ app/Http/Controllers/CustomerLog.php
@@ -31,7 +31,6 @@ class CustomerLog extends Controller
     $log->update([
 
         'log_status'=> 2,
-        'log_payment_method'=>'Credit',
 
     ]);
     $cus_info = CustomerAcc::where('customer_id',$log->customer_id)->first();
