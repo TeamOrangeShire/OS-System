41b4f3b385a2c3086131d352334c6d946ab98f60
JpUbas <162091712+JpUbas@users.noreply.github.com>
2024-05-22 14:17:04 +0800

    change daypass

--- app/Http/Controllers/CustomerLog.php
+++ app/Http/Controllers/CustomerLog.php
@@ -199,6 +199,14 @@ public function LogToPending(Request $request) {
     
         ]);
         
+      }else if(explode('-',$logs->log_transaction)[1] == 3 ){
+         $logs->update([
+
+          'log_status'=> 1,
+          'log_end_time'=> $current,
+          
+        ]);
+        $paymentPass = explode('-',$logs->log_transaction)[0];
       }else{
         $logs->update([
 
@@ -517,9 +525,9 @@ public function AccLogin(Request $request){
         $insertnewlog->customer_id = $insertnew->customer_id;
         $insertnewlog->log_date = now()->setTimezone('Asia/Hong_Kong')->format('d/m/Y');
         $insertnewlog->log_start_time = $startTimeFormatted;
-        $insertnewlog->log_end_time = $endTime;
-        $insertnewlog->log_transaction = $payment.'-0';
-        $insertnewlog->log_status = 1;
+        $insertnewlog->log_end_time = null;
+        $insertnewlog->log_transaction = $payment.'-3';
+        $insertnewlog->log_status = 0;
         $insertnewlog->log_type= 1;
         $insertnewlog->save();
 
@@ -773,9 +781,9 @@ public function logAsDayPass(Request $request){
         $insertnewlog->customer_id = $request->id;
         $insertnewlog->log_date = now()->setTimezone('Asia/Hong_Kong')->format('d/m/Y');
         $insertnewlog->log_start_time = $startTimeFormatted;
-        $insertnewlog->log_end_time = $endTime;
-        $insertnewlog->log_transaction = $payment.'-0';
-        $insertnewlog->log_status = 1;
+        $insertnewlog->log_end_time = null;
+        $insertnewlog->log_transaction = $payment.'-3';
+        $insertnewlog->log_status = 0;
         $insertnewlog->log_type= 1;
         $insertnewlog->save();
 
