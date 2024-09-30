c0fb9d78ea9418dff96d7f84f18438f1b02d1fce
RheyanJohn <rjvblanco.chmsu@gmail.com>
2024-09-30 20:36:11 +0800

    Update reservation UI

--- public/js/booking.js
+++ public/js/booking.js
@@ -23,22 +23,52 @@ $(document).ready(function() {
         ],
         'eventDisplayDefault': false,
         'eventListToggler': false,
-        onSelecteDate: (event, date) => {
-            const selectedDate = new Date(date);
-            selectedDate.setHours(0, 0, 0, 0);
-            // Disable past dates and Sundays
-            if (selectedDate < today || selectedDate.getDay() === 0) {
-                alert("Past dates and Sundays cannot be selected.");
-                return false; // Disable click action for past dates and Sundays
-            }
-        }
     });
-
-});
-
-$('#calendars').on('selectDate', function(event, newDate, oldDate) {
     disablePastDates(today);
 });
+
+$('#calendars').on('selectDate', function(event, newDate) {
+    disablePastDates(today);
+    // Parse the selected date string into a Date object
+    const [month, day, year] = newDate.split("/").map(Number);
+    const selectedDate = new Date(year, month - 1, day); // month is 0-indexed
+
+    // Get the day of the week for the selected date (0 = Sunday, 6 = Saturday)
+    const dayOfWeek = selectedDate.getDay();
+
+    let reserveButton = '';
+    console.log(dayOfWeek);
+    switch(dayOfWeek){
+        case 1:
+            reserveButton = AddReservationButtons(1);
+            break;
+        case 2:
+            reserveButton = AddReservationButtons(2);
+            break;
+        case 3:
+            reserveButton = AddReservationButtons(3);
+            break;
+        case 4:
+            reserveButton = AddReservationButtons(4);
+            break;
+        case 5:
+            reserveButton = AddReservationButtons(5);
+            break;
+        case 6:
+            reserveButton = AddReservationButtons(6);
+            break;
+        default:
+            console.log("invalid date")
+            break;
+    }
+
+    const div = document.getElementById('reservationButtons');
+    div.scrollTop = 0;
+    div.innerHTML = '';
+    div.innerHTML = reserveButton;
+});
+
+
 $('#calendars').on('selectMonth', function() {
     disablePastDates(today);
 });
@@ -46,7 +76,7 @@ $('#calendars').on('selectMonth', function() {
 $('#calendars').on('selectYear', function() {
     setTimeout(() => {
         disablePastDates(today);
-    }, 100); // Wait for the calendar to update
+    }, 100);
 });
 
 // Function to disable past dates and Sundays by adding a custom class
@@ -66,4 +96,141 @@ function disablePastDates(today) {
             });
         }
     });
+}
+
+
+function AddReservationButtons(num){
+
+    let buttons = '';
+
+    const mondayTime = [
+        '08:00 AM',
+        '09:00 AM',
+        '10:00 AM',
+        '11:00 AM',
+        '12:00 PM',
+        '01:00 PM',
+        '02:00 PM',
+        '03:00 PM',
+        '04:00 PM',
+        '05:00 PM',
+        '06:00 PM',
+        '07:00 PM',
+        '08:00 PM',
+        '09:00 PM',
+        '10:00 PM',
+        '11:00 PM',
+        '12:00 AM',
+    ];
+
+    const nonMondayTime = [
+        '01:00 AM',
+        '02:00 AM',
+        '03:00 AM',
+        '04:00 AM',
+        '05:00 AM',
+        '06:00 AM',
+        '07:00 AM',
+        '08:00 AM',
+        '09:00 AM',
+        '10:00 AM',
+        '11:00 AM',
+        '12:00 PM',
+        '01:00 PM',
+        '02:00 PM',
+        '03:00 PM',
+        '04:00 PM',
+        '05:00 PM',
+        '06:00 PM',
+        '07:00 PM',
+        '08:00 PM',
+        '09:00 PM',
+        '10:00 PM',
+        '11:00 PM',
+        '12:00 AM',
+    ];
+
+    const saturdayTime= [
+        '01:00 AM',
+        '02:00 AM',
+        '03:00 AM',
+        '04:00 AM',
+        '05:00 AM',
+        '06:00 AM',
+        '07:00 AM',
+        '08:00 AM',
+        '09:00 AM',
+        '10:00 AM',
+        '11:00 AM',
+        '12:00 PM',
+        '01:00 PM',
+        '02:00 PM',
+        '03:00 PM',
+        '04:00 PM',
+        '05:00 PM',
+        '06:00 PM',
+        '07:00 PM',
+        '08:00 PM',
+        '09:00 PM',
+        '10:00 PM',
+        '11:00 PM',
+        '12:00 AM',
+        '01:00 AM',
+        '02:00 AM',
+        '03:00 AM',
+    ];
+    const add = (delay, data) => {
+        return `<div onclick="selectReservationTime(this)" class="w-100 d-flex p-1 gap-1">
+            <button class="btn btn-primary w-100 wow fadeInLeft select" data-wow-delay="${delay}s">${data}</button>
+            <button class="btn btn-info d-none nextBtn" data-bs-toggle="modal" data-bs-target="#reserveModal" style="width:0%;">Proceed</button>
+        </div>`
+   }
+
+    let delay = 0.01;
+    if(num == 1){
+
+        mondayTime.forEach( data => {
+            buttons += add(delay, data);
+        });
+    }
+
+    if(num == 2 || num == 3 || num == 4 || num == 5){
+        nonMondayTime.forEach( data => {
+            buttons += add(delay, data);
+        });
+    }
+
+    if(num == 6){
+        saturdayTime.forEach( data => {
+            buttons += add(delay, data);
+
+        });
+    }
+
+
+    return buttons;
+}
+
+
+function selectReservationTime(element){
+
+    const allSelect = document.querySelectorAll('.select');
+
+    allSelect.forEach(data=> {
+       const classList = data.classList;
+       const array = Array.from(classList);
+
+       if(array.includes('w-50')){
+        data.nextElementSibling.classList.add('d-none');
+        data.nextElementSibling.style.width ="0";
+        data.classList.add('w-100');
+        data.classList.remove('w-50');
+       }
+    });
+
+    element.children[0].classList.remove('w-100');
+    element.children[0].classList.add('w-50');
+
+    element.children[1].classList.remove('d-none');
+    element.children[1].style.width = "50%";
 }
--- resources/views/homepage/book.blade.php
+++ resources/views/homepage/book.blade.php
@@ -7,6 +7,14 @@
     <link href="{{ asset('calendar/css/evo-calendar.orange-coral.min.css') }}" rel="stylesheet">
     <link rel="stylesheet" href="/css/booking.css">
 </head>
+<style>
+    .select{
+        transition: width 0.3s ease-in-out;
+    }
+    .nextBtn{
+        transition: width 0.3s ease-in-out;
+    }
+</style>
 <body>
     <div class="reservationLoading" id="reservationLoading">
         <div class="loader"></div>
@@ -35,7 +43,15 @@
                     <p>Select room and select prefer date in the calendar to book reservation and proceed filling up the form below it</p>
                 </div>
 
-                <div id="calendars"></div>
+                <div class="row">
+                    <div class=" col-md-9 col-12 ">
+                        <div id="calendars"></div>
+                    </div>
+                    <div class="col-md-3 col-12 d-flex flex-column" style="height: 80vh;overflow-y: auto;" id="reservationButtons">
+
+                    </div>
+
+                </div>
 
                 </div>
 
@@ -43,6 +59,66 @@
             </div>
         </div>
 
+
+        <!-- Modal -->
+<div class="modal fade" id="reserveModal" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-xl">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5" id="staticBackdropLabel">Complete Reservation</h1>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            <div class="row w-100">
+                <div class="col-12 col-md-4">
+                    <h3> Details</h3>
+
+                    <p> <i class="fa-regular fa-clock"></i> Time</p>
+                    <p><i class="fa-solid fa-calendar-check"></i> Date</p>
+                    <br>
+                    <h5>Reminders</h5>
+                    <p> <i class="fa fa-info"></i> Please ensure all details are correct before submitting your reservation. Choose whether you're reserving an office room or a hot desk. Select the date, time, and duration of your booking. Don’t forget to provide your contact information, and if applicable, include any specific room preferences or special requests. Double-check your details to avoid any mistakes. Once everything is complete, click the submit button to confirm your reservation. </p>
+                </div>
+
+                <div class="col-12 col-md-8">
+                    <h3> Fill up form </h3>
+                    <div class="d-flex flex-column gap-1">
+                        <div class="form-floating">
+                            <input type="text" class="form-control" id="name" name="name" placeholder="Your Name">
+                            <label for="name">Your Name</label>
+                        </div>
+                        <div class="form-floating">
+                            <input type="text" class="form-control" id="name" name="name" placeholder="Your Name">
+                            <label for="name">Email</label>
+                        </div>
+                        <div class="form-floating">
+                            <input type="text" class="form-control" id="name" name="name" placeholder="Your Name">
+                            <label for="name">Contact</label>
+                        </div>
+                        <div class="d-flex w-100 justify-content-end">
+                            <button type="button" class="btn btn-primary rounded-pill" data-bs-dismiss="modal">Add Guest Emails</button>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="selectReserve">What do you want to reserve?</label>
+                            <select class="form-select" aria-label="What do you want to reserve?" id="selectReserve">
+                                <option selected disabled>-----Reserve-----</option>
+                                <option value="1">Hotdesk</option>
+                                <option value="2">Room</option>
+                                <option value="3">Three</option>
+                            </select>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Understood</button>
+        </div>
+      </div>
+    </div>
+  </div>
         @include('homepage/Components/footer')
         <!-- Footer End -->
 
