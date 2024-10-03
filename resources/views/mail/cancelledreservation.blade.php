a6110956664dfb04617e95c53e9e7ac846becfa3
RheyanJohn <rjvblanco.chmsu@gmail.com>
2024-10-03 23:55:03 +0800

    Update Design in cancellation Page

--- resources/views/mail/cancelledreservation.blade.php
+++ resources/views/mail/cancelledreservation.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Cancel Reservation - Orange Shire</title>
     <link rel="stylesheet" href="/css/thankyou.css">
 </head>
 <body>
